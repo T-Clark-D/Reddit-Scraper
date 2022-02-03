b798da63a26181f4c39fcfe73a4b5caeeef2f8af
James <jesseserre@gmail.com>
2022-02-02 21:23:01 -0500

    nothin crazy like all dat

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import requests
 
 
 def main():
-    red = requests.get('https://www.reddit.com/r/softwaregore/')
+    #red = requests.get('https://old.reddit.com/r/softwaregore/')
+    red = requests.get('https://old.reddit.com/r/softwaregore/comments/shv0kj/pikmin_bloom_gone_wrong/')
     soup = bs(red.text, 'html.parser')
 
+    var = []
+    #for link in soup.find_all('a'):
+     #   if 'https://old.reddit.com/r/softwaregore/comments/' in str(link):
+      #      var.append(link.get('href'))
+
+
     print(soup.prettify())
+    print(var)
 
 # Run main function
 if __name__ == '__main__':
